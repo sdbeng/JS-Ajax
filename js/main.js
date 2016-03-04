09202e7b2aaa102eb931fb95a3a8bb4538fe82eb
Sergio Barrera <sdbeng@gmail.com>
2016-03-03 20:07:47 -0800

    Clean up tabs and spaces

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
+// $(document).ready(function(){
+// 	console.log("Hello Ajax.");
+// 	$("#getMessage").on("click", function(){
+// 			console.log("Btn clicked.");
+// 			// $(".message").text("I've set a new message today.");
+// 			// $(".message").html("Here is the message.");
+// 			$.getJSON("/api/json/cats.json", function(json){
+// 				$(".message").html(JSON.stringify(json));
+// 			});
+
+// 	});
+
+// });
+
+//convert json to data
+
 $(document).ready(function(){
 	console.log("Hello Ajax.");
 	$("#getMessage").on("click", function(){
 			console.log("Btn clicked.");
-			// $(".message").text("I've set a new message today.");
-			// $(".message").html("Here is the message.");
 			$.getJSON("/api/json/cats.json", function(json){
-				$(".message").html(JSON.stringify(json));
-			});
+				var html = "";
+				//loop through the json data
+				json.forEach(function(val) {
+          			var keys = Object.keys(val);
+          			html += "<div class = 'cat'>";
+          			keys.forEach(function(key) {
+          				html += "<b>" + key + "</b>: " + val[key] + "<br>";
+          			});
+          			html += "</div><br>";
+        		});
+        		$(".message").html(html);
+			});//getJSON
 
 	});
 
