12fcc390325e0e5c85245df0c0ae66c386f5c225
Sergio Barrera <sdbeng@gmail.com>
2016-03-03 18:45:59 -0800

    When btn is clicked show new msg- use html()

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	console.log("Hello Ajax.");
 	$("#getMessage").on("click", function(){
 			console.log("Btn clicked.");
-			$(".message").text("I've set a new message today.");
+			// $(".message").text("I've set a new message today.");
+			$(".message").html("Here is the message.");
 
 	});
 
